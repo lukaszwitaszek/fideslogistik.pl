762bf5a77dcd97fa4f7c5cc23a45d3890ae06e7e
Łukasz Witaszek <lukasz.witaszek@gmail.com>
2017-03-03 17:46:43 +0100

    karuzela dla firm kooperujących

--- public_html/js/my.js
+++ public_html/js/my.js
@@ -1,3 +1,30 @@
-$(function(){
-    $('.selectpicker').selectpicker();
+
+/*
+ * £ukasz Witaszek
+ * Copyright 2017 web.witaszki.pl
+ */
+
+
+
+// Instantiate the Bootstrap carousel
+// http://codepen.io/mephysto/pen/ZYVKRY
+
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
 });
